60bd9247ecf6c9adfaae35cb3083beeedeb213c0
Oliver Körber <Oli@Olivers-MBP.fritz.box>
2015-06-13 18:48:01 +0200

    Initial Commit

---
+++ ZipCashLockable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZipCashLockable
+//
+//  Created by Oliver Körber on 13/06/15.
+//  Copyright (c) 2015 Oliver Körber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZipCashLockableTests/ZipCashLockableTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZipCashLockableTests.swift
+//  ZipCashLockableTests
+//
+//  Created by Oliver Körber on 13/06/15.
+//  Copyright (c) 2015 Oliver Körber. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ZipCashLockableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
